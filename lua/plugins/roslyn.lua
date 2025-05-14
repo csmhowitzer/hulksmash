ba4bff8c9b554197733aeb569c71f12a11cd5baa
csmhowitzer <csmhowitzer@gmail.com>
2025-05-14 10:55:18 -0400

    Added some roslyn specific settings

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
   {
     "seblyng/roslyn.nvim",
@@ -7,8 +8,30 @@ return {
     opts = {
       -- your configuration comes here; leave empty for default settings
       -- NOTE: You must configure `cmd` in `config.cmd` unless you have installed via mason
+      -- for projects located outside of the target (not a child of the root)
+      config = {
+        settings = {
+          ["csharp|completion"] = {
+            dotnet_provide_regex_completions = true,
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+          },
+          ["csharp|symbol_search"] = {
+            dotnet_search_reference_assemblies = true,
+          },
+          ["csharp|formatting"] = {
+            dotnet_organize_imports_on_format = true,
+          },
+          ["navigation"] = {
+            dotnet_navigate_to_decompiled_sources = true,
+          },
+        },
+      },
+      broad_search = true,
     },
-    -- for projects located outside of the target (not a child of the root)
-    broad_search = true,
   },
 }
